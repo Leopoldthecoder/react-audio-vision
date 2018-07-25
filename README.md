2013e10b22718c120aed5bcce29198abcea0f208
Leopoldthecoder <Leopoldthecuber@gmail.com>
2018-07-25 15:54:22 +0800

    minify umd bundle

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-# react-audio-vision [WIP]
+# react-audio-vision
+
+### Demo
+https://jsfiddle.net/leopoldthecuber/2dovasye/
+
+### Install
+
+#### npm
+```bash
+npm install react-audio-vision
+```
+
+#### script tag
+```html
+<script src="https://unpkg.com/react-audio-vision"></script>
+```
+
+### Usage
+
+#### npm
+```js
+import React, { Component } from 'react'
+import AudioVision from 'react-audio-vision'
+
+class App extends Component {
+  render() {
+    return (
+      <AudioVision />
+    )
+  }
+}
+```
+
+#### script tag
+```js
+class App extends React.Component {
+  render() {
+    return (
+      <AudioVision />
+    )
+  }
+}
+```
+
+### API
+```html
+<AudioVision
+  // audio source, can be a string (a url that triggers an XMLHttpRequest) or a blob
+  // 音频源，字符串（音频的 url 地址，会触发 XMLHttpRequest 请求）或 Blob 类型
+  src={src}
+
+  // canvas width, number
+  // canvas 的宽度，数字类型
+  width={500}
+
+  // canvas height, number
+  // canvas 的高度，数字类型
+  height={250}
+
+  // number of bars, must be a power of two in the range of [16, 16384]
+  // 色块的数量，必须是在 [16, 16384] 范围内的 2 的幂
+  bars={128}
+
+  // color of bars, can be a string or a string array
+  // 色块的颜色，字符串或字符串数组
+  barColor={['crimson', 'gold']}
+
+  // pause the audio or not, boolean
+  // 是否暂停播放，布尔型
+  pause={pause}
+
+  // audio play volume, number between 0 and 1
+  // 音频播放音量，介于 0 和 1 之间的数字
+  volume={0.6} />
+```
--- build/rollup.lib.js
+++ build/rollup.lib.js
@@ -3,27 +3,24 @@ const eslint = require('rollup-plugin-eslint').eslint
 const babel = require('rollup-plugin-babel')
 const cjs = require('rollup-plugin-commonjs')
 const resolve = require('rollup-plugin-node-resolve')
+const uglify = require('rollup-plugin-uglify').uglify
 
-const inputOptions = {
-  input: 'src/react-audio-vision.js',
-  plugins: [
-    eslint(),
-    babel({
-      babelrc: false,
-      presets: [ ['@babel/preset-env', { modules: false }], '@babel/preset-react' ],
-      plugins: ['@babel/plugin-proposal-class-properties']
-    }),
-    cjs({
-      namedExports: {
-        'node_modules/react/index.js': ['Component', 'StrictMode']
-      }
-    }),
-    resolve({
-      browser: true,
-    })
-  ],
-  external: ['react', 'react-dom']
-}
+const plugins = [
+  eslint(),
+  babel({
+    babelrc: false,
+    presets: [ ['@babel/preset-env', { modules: false }], '@babel/preset-react' ],
+    plugins: ['@babel/plugin-proposal-class-properties']
+  }),
+  cjs({
+    namedExports: {
+      'node_modules/react/index.js': ['Component', 'StrictMode']
+    }
+  }),
+  resolve({
+    browser: true,
+  })
+]
 
 const outputOptions = {
   name: 'AudioVision',
@@ -34,10 +31,14 @@ const outputOptions = {
 }
 
 const build = async config => {
-  const bundle = await rollup.rollup(inputOptions)
+  const bundle = await rollup.rollup({
+    input: 'src/react-audio-vision.js',
+    plugins: config.uglify ? plugins.concat(uglify()) : plugins,
+    external: ['react', 'react-dom']
+  })
   await bundle.write(Object.assign(outputOptions, config))
 }
 
 build({ file: 'lib/index.common.js', format: 'cjs' })
-build({ file: 'lib/index.umd.js', format: 'umd' })
+build({ file: 'lib/index.umd.js', format: 'umd', uglify: true })
 build({ file: 'lib/index.es.js', format: 'es' })
